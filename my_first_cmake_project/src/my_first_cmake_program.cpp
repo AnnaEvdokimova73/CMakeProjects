51a8e36a11c9582f393709ded76b477dbe493787
Anna Evdokimova <evdockimova.annie@yandex.ru>
2024-02-01 21:09:24 +0300

    Initial commit

---
+++ my_first_cmake_project/src/my_first_cmake_program.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::string name = "";
+    std::cout << "Enter your name: ";
+    std::cin >> name;
+    std::cout << "Hello, " << name << "!" << std::endl;
+}
